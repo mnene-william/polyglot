648045d51a225262299a89b13b5152053b53e5ff
mnene-william <mnenewilliam26@gmail.com>
2025-08-18 04:59:02 -0600

    Make and migrate the language model

--- myapp/models.py
+++ myapp/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Language(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    code = models.CharField(max_length=10, unique=True)
+
+    def __str__ (self):
+        return self.name
