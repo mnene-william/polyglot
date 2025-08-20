6a9f9dedf8116c425073db9d27cfd2ddf45eba27
mnene-william <mnenewilliam26@gmail.com>
2025-08-20 08:32:55 +0300

    create functions for the app

--- myapp/views.py
+++ myapp/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.contrib.auth.decorators import login_required
+from .models import *
 
 # Create your views here.
+def home(request):
+    langauges = Language.objects.all()
+    return render(request, 'home.html', {'languages':langauges})
+
+def language_detail(request, pk):
+    language = get_object_or_404(Language, pk=pk)
+    return render(request, 'language_detail.html', {'language:':language, 'lesson':language.lesson.all()})
+
